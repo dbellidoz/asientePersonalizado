fee926bc9d213ced3259563d767cc4d5c2222a6e
Daniel <dbellidoz@gmail.com>
2026-04-08 14:20:18 +0200

    avanza en vectorstore

--- src/main/java/com/localagent/RAG/VectorStore.java
+++ src/main/java/com/localagent/RAG/VectorStore.java
@@ -12,23 +12,31 @@ public class VectorStore {
     private List<Chunk> chunksList = new ArrayList<>();
     private List<Chunk> chunksInput;
     
+    //Metodo que añade los chucks de consulta
     public void addNewChunkDocumento(Chunk chunk){
         this.chunksList.add(chunk);
     }
-/* 
-    public double[] similarity(double[] embInput){
-        List<AbstractMap.SimpleEntry<Chunk, Double>> similaridad = new ArrayList();
-        
+ 
+    public List<AbstractMap.SimpleEntry<Chunk, Double>> similarity(double[] embInput){
+        List<AbstractMap.SimpleEntry<Chunk, Double>> similaridades = new ArrayList<>();
+        for (int i = 0; i < chunksList.size(); i++) {
+            Double similaridad = cosineSimilarity(embInput, chunksList.get(i).getEmbedding());
+            similaridades.add(new AbstractMap.SimpleEntry<>(chunksList.get(i), similaridad));
+        }
 
-        
-    }*/
+        return similaridades;
+    }
 
+    //Metodo privado que calcula la similaridad entre dos chunks (consulta - documentos)
+    //Parametro 1: valores embebidos de la consulta
+    //Parametro 2: valores embebidos de los documentos
     private double cosineSimilarity(double[] inputEmb, double[] chunksListEmb){
         double puntoProducto = 0.0;
         double magnitudA = 0.0;
         double magnitudB = 0.0;
         double similaridad = 0.0;
 
+        //suma de los cuadrados de los valores embebidos
         for (int i = 0; i < inputEmb.length; i++) {
             puntoProducto += inputEmb[i] * chunksListEmb[i];
             magnitudA += inputEmb[i] * inputEmb[i];
